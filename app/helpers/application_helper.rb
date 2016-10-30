1393f06e088510ad30570888b61c0008a708a0e6
Bearded Avenger <email@nickhaskins.com>
2016-10-30 11:26:22 -0400

    proper page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
-
+	def title(*page_title)
+    if Array(page_title).size.zero?
+      content_for?(:title) ? content_for(:title) : t(:site_name)
+    else
+      content_for :title, (Array(page_title) << t(:site_name)).join(' - ')
+    end
+  end
 end
